f002a46a698532897cf346367bcbc6fea9aecb37
Rippan <jepperipnas@gmail.com>
2020-12-19 23:57:07 +0100

    added bcryptJS for hashing passwords for new users

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require ('express').Router();
 const User = require('../model/User');
 const { regValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
 
@@ -11,15 +12,20 @@ router.post('/register', async (req, res) => {
     // CHECK IF EMAIL ALREADY EXIST IN DATABASE
     const checkIfEmailExist = await User.findOne({ email: req.body.email});
     if (checkIfEmailExist) return res.status(400).send('Email already exists');
+
+    // HASH PASSWORDS
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
     
+    // OBJECT REQUIREMENTS FOR NEW USERS
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user:user._id});
     }catch(err){
         res.status(400).send(err);
     }
