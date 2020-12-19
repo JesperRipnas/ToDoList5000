f20a371d93aa1e2a17a243ea611fe185a3203b33
Rippan <jepperipnas@gmail.com>
2020-12-19 22:36:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to db')
+);
+
+// Middleware
+app.use(express.json());
+
+// Route Middlewares
+app.use('/api/user', authRoute);
+
+// Host server at port 8080
+app.listen(8080, () => console.log('Server running'));
+
